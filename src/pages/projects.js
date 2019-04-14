628d696ff17ffeda46cafa4f8b7535e31dce9aef
Misha Pachin <misha.pachin@gmail.com>
2019-04-14 14:53:35 +0300

    Update projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -21,18 +21,11 @@ const PortfolioPage = ({
 
         <div style={{ marginTop: rhythm(2) }}>
             <p>
+                Currently, I'm working on <a target="_blank" href="https://github.com/react-ref-groupie">react-ref-groupie</a> project. This is a React refs manager library which allows you to handle complex animations/ref manipulation logic independently from component nesting.
+            </p>
+            <p>
                 One of projects deserved to be mentioned is this blog itself, its repository placed on <a target="_blank" href="https://github.com/mpachin/tech-agnostic.me">GitHub</a>.
             </p>
-            <p>
-                Another one is my <a target="_blank" href="https://codepen.io/misha_pachin/pen/OZWjZr">Game of Life</a> implementation,
-                created as part of <a target="_blank" href="https://www.freecodecamp.org/">freeCodeCamp</a> tasks.
-            </p>
-            <p>
-                Currently I'm looking into contributing to open source, I'll update this page with relevant info. For the time of writing PR's worth mentioning is:
-            </p>
-            <ul>
-                <li><a target="_blank" href="https://github.com/react-cosmos/react-cosmos/pull/689">react-cosmos's support for node_modules subdirectories</a></li>
-            </ul>
         </div>
     </Layout>
 );
